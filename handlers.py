3f11995d4cac71c67a5d240694c118033bf6b4f9
Smaxerr <smax1027@icloud.com>
2025-07-17 23:05:22 +0100

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -225,6 +225,30 @@ async def handle_card_list(message: Message, state: FSMContext):
     await message.answer("✅ All done.")
     await state.clear()
 
+# ===== Take screenshot using Playwright =====
+async def take_royalmail_screenshot(card: str) -> str:
+    filename = f"screenshots/{uuid.uuid4()}.png"
+    os.makedirs("screenshots", exist_ok=True)
+
+    try:
+            
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=True)
+            page = await browser.new_page()
+            await page.goto("https://send.royalmail.com/send/youritem?country=GBR&format&weight=&weightUnit=G", timeout=60000)
+            await page.wait_for_timeout(2000)  # Wait for full page load
+
+
+            await page.screenshot(path=filename, full_page=True)
+            await browser.close()
+        return filename
+    except Exception as e:
+        print(f"[RoyalMail Screenshot Error]: {e}")
+        return None
+
+
+
+
 
 @router.callback_query(F.data == "back_main")
 async def back_main(cb: CallbackQuery):
