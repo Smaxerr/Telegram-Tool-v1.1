4292c9a2ce57df8cdab437c7960d71bc20d74377
Smaxerr <smax1027@icloud.com>
2025-07-17 22:43:46 +0100

    Update ovo.py

--- ovo.py
+++ ovo.py
@@ -1,28 +1,24 @@
 from playwright.async_api import async_playwright
 from playwright_stealth import stealth_async
 
-async def screenshot_ovo(output_path: str = "/tmp/ovo_page.png"):
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(
-            headless=True,
-            args=[
-                "--disable-blink-features=AutomationControlled",
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-            ]
-        )
-        page = await browser.new_page()
+# ===== Take screenshot using Playwright =====
+async def take_royalmail_screenshot(card: str) -> str:
+    filename = f"screenshots/{uuid.uuid4()}.png"
+    os.makedirs("screenshots", exist_ok=True)
 
-        # Apply stealth here
-        await stealth_async(page)
+    try:
+            
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=True)
+            page = await browser.new_page()
+            await page.goto("https://send.royalmail.com/send/youritem?country=GBR&format&weight=&weightUnit=G", timeout=60000)
+            await page.wait_for_timeout(2000)  # Wait for full page load
 
-        # Optional: set realistic user agent
-        await page.set_user_agent(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-            "(KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
-        )
+            
 
-        await page.goto("https://ovoenergypayments.paypoint.com/Guestpayment")
-        await page.screenshot(path=output_path, full_page=True)
-        await browser.close()
-    return output_path
+            await page.screenshot(path=filename, full_page=True)
+            await browser.close()
+        return filename
+    except Exception as e:
+        print(f"[RoyalMail Screenshot Error]: {e}")
+        return None
