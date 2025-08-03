29db20622a34a5ff4f59c29a4e13196d2151de71
Smaxerr <smax1027@icloud.com>
2025-08-03 18:40:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # This loads environment variables from .env file
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+ADMIN_IDS = list(map(int, filter(None, os.getenv("ADMIN_IDS", "").split(","))))
+
 
 DB_CONFIG = {
     "user": os.getenv("PGUSER"),
