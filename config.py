065784ccb158331bdf7d8e4fd935a1ebd9eb4013
Smaxerr <smax1027@icloud.com>
2025-08-03 18:48:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ load_dotenv()  # This loads environment variables from .env file
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_IDS = list(map(int, filter(None, os.getenv("ADMIN_IDS", "").split(","))))
 
-
 DB_CONFIG = {
-    "user": os.getenv("PGUSER"),
-    "password": os.getenv("PGPASSWORD"),
-    "database": os.getenv("PGDATABASE"),
-    "host": os.getenv("PGHOST"),
-    "port": int(os.getenv("PGPORT", 5432)),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT", 5432)),
 }
+
