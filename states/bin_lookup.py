ed5454285c883f93683eba6299f5865a43c687b2
Smaxerr <smax1027@icloud.com>
2025-08-04 18:02:07 +0100

    Update bin_lookup.py

--- states/bin_lookup.py
+++ states/bin_lookup.py
@@ -1,9 +1,12 @@
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 
+class ccformatterstate(StatesGroup):
+    ccformatter = State()
 
 class BinLookupState(StatesGroup):
     waiting_for_bin = State()
 
 class RoyalMailStates(StatesGroup):
     awaiting_cards = State()
+
