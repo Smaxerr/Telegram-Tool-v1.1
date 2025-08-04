5aa76a93b51e502169a1608c0facec8a4f9aa851
Smaxerr <smax1027@icloud.com>
2025-08-04 17:56:55 +0100

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -4,6 +4,7 @@ def main_menu():
     kb = InlineKeyboardBuilder()
     kb.button(text="🔍Advanced BIN Lookup", callback_data="BINlookup")
     kb.button(text="⚡OvO Charger", callback_data="royalmail_charger")
+    kb.button(text="🧹Card Formatter", callback_data="ccformatter")
     kb.adjust(1)  # Each button takes 1 row
     return kb.as_markup()
 
