10aa7422cfc37fee130dd03f9e68a1f5342f5a3a
Smaxerr <smax1027@icloud.com>
2025-08-05 17:26:51 +0100

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -217,7 +217,7 @@ async def start_bin_lookup(callback: CallbackQuery, state: FSMContext):
 @router.callback_query(F.data == "royalmail_charger")
 async def royalmail_callback(callback: CallbackQuery, state: FSMContext):
     await state.set_state(RoyalMailStates.awaiting_cards)
-    await callback.message.answer("Please send the card(s) you'd like to check, one per line:\n Format: cardnumber|expmonth|expyear|cvv")
+    await callback.message.answer("💳 Please send the card(s) you'd like to check, one per line.\nFormat: cardnumber|expmonth|expyear|cvv\n🤝 Each check will cost 1 credit. ")
     await callback.answer()
 
 
