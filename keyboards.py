ccd6fa17ccb96229de94aecce005f5532b25faee
Smaxerr <smax1027@icloud.com>
2025-08-06 23:14:48 +0100

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,9 +5,6 @@ def main_menu():
     kb = InlineKeyboardBuilder()
     kb.button(text="🔍Advanced BIN Lookup", callback_data="BINlookup")
     kb.button(text="⚡OvO Charger", callback_data="ovo_charger")
-    kb.button(text="⚡RM Charger (pending)", callback_data="rm_charger")
-    kb.button(text="🧹Card Formatter (pending)", callback_data="ccformatter")
-    kb.button(text="🔢 CC Count Checker (pending)", callback_data="bincountchecker")
     kb.button(text="⚙️Settings (pending)", callback_data="settings")
     kb.button(text="🛍️Card Store", callback_data="secret")
     kb.adjust(1)  # Each button takes 1 row
